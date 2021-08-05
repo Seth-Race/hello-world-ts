a06113d06cef66407487e5a49672feb44c0c822c
Seth-Race <sethrace@gmail.com>
2021-08-05 09:52:23 -0400

    Init commit

---
+++ js/hello-world.js
@@ -0,0 +1,4 @@
+"use strict";
+var hw = "Hello world!";
+var myName = "Seth";
+console.log(hw, myName);
---
+++ ts/hello-world.ts
@@ -0,0 +1,3 @@
+let hw:string = "Hello world!";
+let myName:string = "Seth";
+console.log(hw, myName);
